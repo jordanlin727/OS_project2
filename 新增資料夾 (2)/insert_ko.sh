6b8c8796c33090efaf49515fe752dfe9d0b85185
james88878 <43361543+james88878@users.noreply.github.com>
2019-06-11 17:10:21 +0800

    Delete insert_ko.sh

--- 新增資料夾 (2)/insert_ko.sh
+++
@@ -1,8 +0,0 @@
-sudo insmod network_server.ko io_mode="mmap"
-echo "server"
-sudo ./master README.md mmap
-echo "master"
-sudo insmod network_client.ko io_mode="mmap"
-echo "client"
-sudo ./slave README_slave.md mmap 192.168.50.145
-echo "slave"
